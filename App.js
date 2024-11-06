1b16311acd5ecd853d4d9beaa173aa7255c51651
Samuel Joseph Koffler <smjobob@gmail.com>
2024-11-06 18:32:19 -0500

    Update App.js
    Added text style change to bottom of code

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ToastAndriod, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Change made by Sam</Text>
+      <Text style={styles.text}>Change made by Sam</Text>
       <Text>Change made by Aditya</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
@@ -19,4 +19,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: "#304ffe",
+    fontSize: 20,
+    padding: 20
+  },
+
+  button: {
+    borderRadius: 10,
+    backgroundColor:"#29b6f6",
+    width: 150,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
