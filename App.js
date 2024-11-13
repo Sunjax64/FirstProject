759a3d0498cd04217184c9e0c993928ae19bad4a
Josh <141881186+Yvble@users.noreply.github.com>
2024-11-13 18:45:52 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import DetailsScreen from './screens/DetailsScreen';
-import AboutScreen from './screens/HomeScreen';
-import HomeScreen from './screens/AboutScreen';
+import HomeScreen from './screens/HomeScreen'; // Corrected import
+import DetailsScreen from './screens/DetailsScreen'; 
+import AboutScreen from './screens/AboutScreen'; // Corrected import
 
 // Create the Stack Navigator
 const Stack = createNativeStackNavigator();
@@ -12,9 +12,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home"   
-      // screenOptions={{ headerShown: false, animation: 'fade'}}
-      >
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="About" component={AboutScreen} />
