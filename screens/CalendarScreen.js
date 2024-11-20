33721e1d9241b57bca644c435ac045f7da45dfaa
Josh <141881186+Yvble@users.noreply.github.com>
2024-11-20 18:50:37 -0500

    Update CalendarScreen.js
    added home and settings

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,18 +1,37 @@
 import React, {useState} from 'react';
 import {Calendar} from 'react-native-calendars';
+import { View, Text, Button } from 'react-native';
 
-const CalendarScreen = () => {
+const CalendarScreen = ({ navigation }) => {
   const [selected, setSelected] = useState('');
 
   return (
-    <Calendar
+    <><Calendar
+      style={{
+        borderWidth: 1,
+        borderColor: 'gray',
+        height: 350,
+      }}
+      theme={{
+        backgroundColor: '#ffffff',
+        calendarBackground: '#ffffff',
+        textSectionTitleColor: '#b6c1cd',
+        selectedDayBackgroundColor: '#00adf5',
+        selectedDayTextColor: '#ffffff',
+        todayTextColor: '#00adf5',
+        dayTextColor: '#2d4150',
+        textDisabledColor: '#dd99ee'
+      }}
+      
       onDayPress={day => {
         setSelected(day.dateString);
-      }}
+      } }
       markedDates={{
-        [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
-      }}
-    />
+        [selected]: { selected: true, disableTouchEvent: true, selectedDotColor: 'orange' }
+      }} />
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button title="Home Screen" onPress={() => navigation.navigate('Home')} />
+      </View></>
   );
 };
 
